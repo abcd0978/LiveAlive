3b3481f024f708ffa33cc5d93b8b283fe703853d
abcd0978 <kimmin36944@gmail.com>
2020-09-09 18:12:23 +0900

    modified

--- Project/TeamProject/src/Application/Appmain.java
+++ Project/TeamProject/src/Application/Appmain.java
@@ -11,16 +11,11 @@ public class Appmain extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("root.fxml"));
 		Scene scene = new Scene(root); // Scene 객체 생성
-		
 		primaryStage.setTitle("살자, 건강하게.");
 		primaryStage.setScene(scene);
-		
 		primaryStage.show();
 	}
-	
 	public static void main(String args[]) {
 		launch(args);
 	}
-	
-
 }
--- Project/TeamProject/src/Application/rootContoroller.java
+++ Project/TeamProject/src/Application/rootContoroller.java
@@ -2,11 +2,24 @@ package Application;
 
 import java.util.ResourceBundle;
 
+import javafx.event.*;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public abstract class rootContoroller implements Initializable {
+public class rootContoroller implements Initializable {
+	@FXML private Button login;
+	@FXML private Button signup;
+	@FXML private TextField user_id;
+	@FXML private TextField user_pass;
 	@Override
-	public void initialize(java.net.URL loaction, ResourceBundle resources) {
-		
+	public void initialize(java.net.URL loaction, ResourceBundle resources) 
+	{
+		login.setOnAction(event->do_action(event));
+	}
+	public void do_action(ActionEvent e)
+	{
+		System.out.println("login pressed");
 	}
 }
